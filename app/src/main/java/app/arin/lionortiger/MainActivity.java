d067e0c4152f009cc450d95414c0e447d69eee59
Arin Y <arinhakoopian@gmail.com>
2018-12-10 16:07:27 -0800

    2nd commit

--- app/src/main/java/app/arin/lionortiger/MainActivity.java
+++ app/src/main/java/app/arin/lionortiger/MainActivity.java
@@ -2,6 +2,7 @@ package app.arin.lionortiger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -10,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     enum Player {
 
-        ONE, TWO
+        ONE, TWO , NoOne
     }
 
     Player currentPlayer = Player.ONE;
@@ -28,31 +29,61 @@ public class MainActivity extends AppCompatActivity {
             {2 , 4 , 6}
     };
 
+    private boolean gameOver = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerChoices[0] = Player.NoOne;
+        playerChoices[1] = Player.NoOne;
+        playerChoices[2] = Player.NoOne;
+        playerChoices[3] = Player.NoOne;
+        playerChoices[4] = Player.NoOne;
+        playerChoices[5] = Player.NoOne;
+        playerChoices[6] = Player.NoOne;
+        playerChoices[7] = Player.NoOne;
+        playerChoices[8] = Player.NoOne;
     }
 
     public void imageViewIsTapped(View imageView) {
         ImageView tappedImageView = (ImageView) imageView;
-
-        tappedImageView.setTranslationX(-2000);
-
         int tiTag = Integer.parseInt(tappedImageView.getTag().toString());
-        playerChoices[tiTag] = currentPlayer;
 
-        if(currentPlayer == Player.ONE) {
-            tappedImageView.setImageResource(R.drawable.lion);
-            currentPlayer = Player.TWO;
-        } else if (currentPlayer == Player.TWO) {
-            tappedImageView.setImageResource(R.drawable.tiger);
-            currentPlayer = Player.ONE;
+        if(playerChoices[tiTag] == Player.NoOne && gameOver == false) {
+            tappedImageView.setTranslationX(-2000);
+
+
+            playerChoices[tiTag] = currentPlayer;
+            if (currentPlayer == Player.ONE) {
+                tappedImageView.setImageResource(R.drawable.lion);
+                currentPlayer = Player.TWO;
+            } else if (currentPlayer == Player.TWO) {
+                tappedImageView.setImageResource(R.drawable.tiger);
+                currentPlayer = Player.ONE;
+            }
+
+            tappedImageView.setBackground(null);
+            tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+
+            Toast.makeText(this, tappedImageView.getTag().toString(), Toast.LENGTH_SHORT).show();
+
+            for (int[] winnerColumns : winnerRowsColumns) {
+                if (playerChoices[winnerColumns[0]] ==
+                        playerChoices[winnerColumns[1]]
+                        && playerChoices[winnerColumns[1]]
+                        == playerChoices[winnerColumns[2]] && playerChoices[winnerColumns[0]] != Player.NoOne) {
+                    gameOver = true;
+                    String winnerOfGame = "";
+                    if (currentPlayer == Player.ONE) {
+                        winnerOfGame = "Player Two";
+                    } else if (currentPlayer == Player.TWO) {
+                        winnerOfGame = "Player One";
+                    }
+                    Toast.makeText(this, winnerOfGame + " is the Winner", Toast.LENGTH_LONG).show();
+                }
+            }
         }
-
-
-        tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
-
-        Toast.makeText(this, tappedImageView.getTag().toString(), Toast.LENGTH_LONG).show();
     }
 }
